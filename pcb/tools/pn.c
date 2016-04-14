032dd3390eeee637b8f3f33231d66b5fc9b45867
jks-prv <jks@jks.com>
2016-04-14 12:10:37 +1200

    add copyright

--- pcb/tools/pn.c
+++ pcb/tools/pn.c
@@ -1,6 +1,26 @@
+/*
+--------------------------------------------------------------------------------
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+You should have received a copy of the GNU Library General Public
+License along with this library; if not, write to the
+Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+Boston, MA  02110-1301, USA.
+--------------------------------------------------------------------------------
+*/
 
+// Copyright (c) 2015-2016 John Seamons, ZL/KF6VO
+
+//
 // Add parts number and manufacturer fields to BOM based on value/footprint association.
 // Resulting file can be uploaded to e.g. octopart.com for quotation.
+//
 
 // octopart.com BOM column assignment order: 5, 2, 7, skip, 3
 
