20b111856aee7f46412a8bd41db1ef06ae4fd047
jks-prv <jks@jks.com>
2016-08-03 14:16:16 +1200

    use v1.0 image

--- tools/kiwiSDR-download-KiwiSDR-create-micro-SD-flasher.sh
+++ tools/kiwiSDR-download-KiwiSDR-create-micro-SD-flasher.sh
@@ -3,15 +3,22 @@
 
 # NB: this distro image is a flasher
 
-VER="v0.20"
-CKSUM="3d8d77715a75c2192dc2cd6401d3024d7a912a6af35f99eaa26418d581811738"
+VER="v1.0"
+DEBIAN_VER="8.4"
+CKSUM="b50e01cba539a8d98aaa8aec30cfbd3ed661f5ebf76a066cab0856424504567c"
+
+#VER="v0.20"
+#DEBIAN_VER="8.4"
+#CKSUM="3d8d77715a75c2192dc2cd6401d3024d7a912a6af35f99eaa26418d581811738"
 
 #VER="v0.12"
+#DEBIAN_VER="8.4"
 #CKSUM="c49956c9514a76166b92e72efb4bd2483e30c999afcf8f613a2192692c03a24d"
 
-#KIWISDR="kiwisdr.com/files"
-KIWISDR="dl.dropboxusercontent.com/u/68809050/KiwiSDR"
-DISTRO="KiwiSDR_${VER}_BBB_Debian_8.4.img.xz"
+# image files are available on both dropbox.com and kiwisdr.com
+#DISTRO_HOST="kiwisdr.com/files"
+DISTRO_HOST="dl.dropboxusercontent.com/u/68809050/KiwiSDR"
+DISTRO="KiwiSDR_${VER}_BBB_Debian_${DEBIAN_VER}.img.xz"
 
 echo "--- get KiwiSDR distro image from net and create micro-SD flasher"
 echo -n "--- hit enter when ready:" ; read
@@ -24,7 +31,7 @@ fi
 
 if test ! -f ${DISTRO} ; then
 	echo "--- getting distro"
-	wget https://${KIWISDR}/${DISTRO}
+	wget https://${DISTRO_HOST}/${DISTRO}
 else
 	echo "--- already seem to have the distro file, verify checksum below to be sure"
 fi
@@ -34,12 +41,13 @@ echo ${CKSUM} " correct checksum"
 echo "--- verify that the two checksums above match"
 echo -n "--- hit enter when ready:" ; read
 
-echo "--- lsblk:"
-lsblk
 echo "--- insert micro-SD card"
 echo -n "--- hit enter when ready:" ; read
+echo "--- lsblk:"
+lsblk
 
 echo "--- copying to micro-SD card, will take several minutes"
+echo -n "--- hit enter when ready:" ; read
 time xzcat -v ${DISTRO} | dd of=/dev/mmcblk1
 
 echo "--- when next booted with micro-SD installed, KiwiSDR image should be copied to Beagle eMMC flash"
