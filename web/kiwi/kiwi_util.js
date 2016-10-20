20d05c850944e4b2af258e653d7ffb7760ebc2cc
jks-prv <jks@jks.com>
2016-10-21 10:55:14 +1300

    add client endian check routine

--- web/kiwi/kiwi_util.js
+++ web/kiwi/kiwi_util.js
@@ -164,6 +164,15 @@ function deleteCookie(cookie)
 	createCookie(cookie, 0, -1);
 }
 
+var littleEndian = (function() {
+	var buffer = new ArrayBuffer(2);
+	new DataView(buffer).setInt16(0, 256, true /* littleEndian */);
+	// Int16Array uses the platform's endianness.
+	return new Int16Array(buffer)[0] === 256;
+})();
+
+console.log('littleEndian='+ littleEndian);
+
 
 // HTML helpers
 
