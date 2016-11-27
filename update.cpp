f18456ae264bc798f2ea59a9a183590be59b941a
Jon Little <burundiocibu@gmail.com>
2016-11-27 14:38:54 -0600

    Making the update process use the repo's upstream and branch

--- update.cpp
+++ update.cpp
@@ -25,6 +25,7 @@ Boston, MA  02110-1301, USA.
 #include "web.h"
 #include "cfg.h"
 #include "coroutines.h"
+#include <string>
 
 #include <types.h>
 #include <unistd.h>
@@ -52,76 +53,99 @@ static void update_build(void *param)
 	exit(0);
 }
 
+
 static void update_task(void *param)
 {
-	int status;
-	bool check_only = (bool) param;
-	
-	lprintf("UPDATE: checking for updates\n");
-	status = system("cd /root/" REPO_NAME "; wget --no-check-certificate https://raw.githubusercontent.com/jks-prv/Beagle_SDR_GPS/master/Makefile -O Makefile.1");
+   using std::string;
 
-	if (status < 0 || WEXITSTATUS(status) != 0) {
-		lprintf("UPDATE: wget Makefile, no Internet access?\n");
-		update_pending = update_in_progress = false;
-		return;
-	}
+   int status;
+   bool check_only = (bool) param;
 	
-	FILE *fp;
-	scallz("fopen Makefile.1", (fp = fopen("/root/" REPO_NAME "/Makefile.1", "r")));
-		int n1, n2;
-		n1 = fscanf(fp, "VERSION_MAJ = %d\n", &pending_maj);
-		n2 = fscanf(fp, "VERSION_MIN = %d\n", &pending_min);
-	fclose(fp);
-	
-	bool ver_changed = (n1 == 1 && n2 == 1 && (pending_maj > VERSION_MAJ  || (pending_maj == VERSION_MAJ && pending_min > VERSION_MIN)));
-	bool update_install = (cfg_bool("update_install", NULL, CFG_REQUIRED) == true);
-	
-	if (check_only && !force_build) {
-		if (ver_changed)
-			lprintf("UPDATE: version changed (current %d.%d, new %d.%d), but check only\n",
-				VERSION_MAJ, VERSION_MIN, pending_maj, pending_min);
-		else
-			lprintf("UPDATE: running most current version\n");
-		update_pending = update_in_progress = false;
-		return;
-	} else
+   lprintf("UPDATE: checking for updates from " REPO "\n");
+   using namespace std;
+   string repo(REPO);
+   string repo_name(REPO_NAME);
+   string branch(BRANCH);
+        
+   if (repo.find("https://github.com/") != 0) {
+      lprintf("UPDATE: Upstream (%s) must be at github to continue with update\n", repo.substr(0,19).c_str());
+      update_pending = update_in_progress = false;
+      return;
+   }
+      // Remove any trailing ".git"
+   size_t p = repo_name.rfind(".git");
+   if (p != string::npos)
+      repo_name.erase(p);
+   p = repo.rfind(".git");
+   if (p != string::npos)
+      repo.erase(p);
 
-	if (ver_changed && !update_install && !force_build) {
-		lprintf("UPDATE: version changed (current %d.%d, new %d.%d), but update install not enabled\n",
-			VERSION_MAJ, VERSION_MIN, pending_maj, pending_min);
-	} else
+   string repo_dir = "/root/" + repo_name;
+   string mfn = repo_dir + "/Makefile.1";
+        
+   string cmd = "wget -q --no-check-certificate https://raw.githubusercontent.com/" +
+      repo.substr(19) + "/" + branch + "/Makefile -O " + mfn;
+   lprintf("UPDATE: %s\n", cmd.c_str());
+   status = system(cmd.c_str());
+  
+   if (status < 0 || WEXITSTATUS(status) != 0) {
+      lprintf("UPDATE: cmd failed\n");
+      update_pending = update_in_progress = false;
+      return;
+   }
 	
-	if (ver_changed || force_build) {
-		lprintf("UPDATE: version changed%s, current %d.%d, new %d.%d\n",
-			force_build? " (forced)":"",
-			VERSION_MAJ, VERSION_MIN, pending_maj, pending_min);
-		lprintf("UPDATE: building new version..\n");
+   FILE *fp;
+   scallz("fopen Makefile.1", (fp = fopen(mfn.c_str(), "r")));
+   int n1, n2;
+   n1 = fscanf(fp, "VERSION_MAJ = %d\n", &pending_maj);
+   n2 = fscanf(fp, "VERSION_MIN = %d\n", &pending_min);
+   fclose(fp);
+   
+   bool ver_changed = (n1 == 1 && n2 == 1 && (pending_maj > VERSION_MAJ  || (pending_maj == VERSION_MAJ && pending_min > VERSION_MIN)));
+   bool update_install = (cfg_bool("update_install", NULL, CFG_REQUIRED) == true);
+   
+   if (check_only && !force_build) {
+      if (ver_changed)
+         lprintf("UPDATE: version changed (current %d.%d, new %d.%d), but check only\n",
+                 VERSION_MAJ, VERSION_MIN, pending_maj, pending_min);
+      update_pending = update_in_progress = false;
+      return;
+   } else
+      
+      if (ver_changed && !update_install && !force_build) {
+         lprintf("UPDATE: version changed (current %d.%d, new %d.%d), but update install not enabled\n",
+                 VERSION_MAJ, VERSION_MIN, pending_maj, pending_min);
+      } else if (ver_changed || force_build) {
+         lprintf("UPDATE: version changed%s, current %d.%d, new %d.%d\n",
+                 force_build? " (forced)":"",
+                 VERSION_MAJ, VERSION_MIN, pending_maj, pending_min);
+         lprintf("UPDATE: building new version..\n");
+         
+            // Run build in a Linux child process so the server can continue to respond to connection requests
+            // and display a "software update in progress" message.
+            // This is because the calls to system() in update_build() block for the duration of the build.
+         status = child_task(SEC_TO_MSEC(1), update_build, (void *) (long) force_build);
+         int exited = WIFEXITED(status);
+         int exit_status = WEXITSTATUS(status);
+         
+         if (! (exited && exit_status == 0)) {
+            if (exited) {
+               lprintf("UPDATE: git pull, no Internet access?\n");
+                  //lprintf("UPDATE: error in build, exit status %d, aborting\n", exit_status);
+            } else {
+               lprintf("UPDATE: error in build, non-normal exit, aborting\n");
+            }
+            update_pending = update_in_progress = false;
+            return;
+         }
 		
-		// Run build in a Linux child process so the server can continue to respond to connection requests
-		// and display a "software update in progress" message.
-		// This is because the calls to system() in update_build() block for the duration of the build.
-		status = child_task(SEC_TO_MSEC(1), update_build, (void *) (long) force_build);
-		int exited = WIFEXITED(status);
-		int exit_status = WEXITSTATUS(status);
-		
-		if (! (exited && exit_status == 0)) {
-			if (exited) {
-				lprintf("UPDATE: git pull, no Internet access?\n");
-				//lprintf("UPDATE: error in build, exit status %d, aborting\n", exit_status);
-			} else {
-				lprintf("UPDATE: error in build, non-normal exit, aborting\n");
-			}
-			update_pending = update_in_progress = false;
-			return;
-		}
-		
-		lprintf("UPDATE: switching to new version %d.%d\n", pending_maj, pending_min);
-		xit(0);
-	} else {
-		lprintf("UPDATE: version %d.%d is current\n", VERSION_MAJ, VERSION_MIN);
-	}
-	
-	update_pending = update_in_progress = false;
+         lprintf("UPDATE: switching to new version %d.%d\n", pending_maj, pending_min);
+         xit(0);
+      } else {
+         lprintf("UPDATE: version %d.%d is current\n", VERSION_MAJ, VERSION_MIN);
+      }
+   
+   update_pending = update_in_progress = false;
 }
 
 // called on each user logout or on demand by UI
