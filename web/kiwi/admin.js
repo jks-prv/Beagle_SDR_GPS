97fa7796d0aecb5c0ac07aa4e09056e62a2988d1
jks-prv <jks@jks.com>
2017-01-01 12:03:24 +1300

    don't show button until a GPS solution

--- web/kiwi/admin.js
+++ web/kiwi/admin.js
@@ -394,7 +394,7 @@ function sdr_hu_html()
 			w3_input('Grid square (4 or 6 char) ', 'rx_grid', '', 'sdr_hu_input_grid', null, null, w3_inline('id-sdr_hu-grid-check cl-admin-check w3-green')),
 			w3_input('Location (lat, lon) ', 'rx_gps', '', 'sdr_hu_check_gps', null, null,
 				w3_inline('id-sdr_hu-gps-check cl-admin-check w3-green') + ' ' +
-				w3_inline('id-sdr_hu-gps-set cl-admin-check w3-blue w3-pointer w3-hide', '', 'set from GPS')
+				w3_divs('id-sdr_hu-gps-set cl-admin-check w3-blue w3-pointer w3-hide', '', 'set from GPS')
 			),
 			w3_input_get_param('Altitude (ASL meters)', 'rx_asl', 'admin_int_cb')
 		) +
