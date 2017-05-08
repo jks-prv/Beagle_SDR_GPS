79094f977359f75a7d7c5b20775259bb01ff5b2a
jks-prv <jks@jks.com>
2017-05-09 10:49:58 +1200

    URL band select also sets band menu

--- web/extensions/wspr/wspr.js
+++ web/extensions/wspr/wspr.js
@@ -351,7 +351,8 @@ function wspr_controls_setup()
 	if (p) {
 		p = p.toLowerCase();
 		if (typeof wspr_freqs_s[p] != 'undefined') {
-			wspr_freq(wspr_freqs_s[p]);
+			w3_set_value('wspr_init_band', wspr_freqs_s[p]);
+			wspr_band_select_cb('wspr_init_band', wspr_freqs_s[p], false);
 		} else {
 			console.log('WSPR ext_param='+ p +' UNKNOWN');
 		}
