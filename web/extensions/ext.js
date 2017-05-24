8fe3ad643647948b6a745a44fd07a268998aecff
jks-prv <jks@jks.com>
2017-05-24 12:17:25 +1200

    ext_set_data_height() adjusts extension data panel height

--- web/extensions/ext.js
+++ web/extensions/ext.js
@@ -27,6 +27,16 @@ function ext_panel_show(controls_html, data_html, show_func)
 	extint_panel_show(controls_html, data_html, show_func);
 }
 
+function ext_set_data_height(height)
+{
+   var el = w3_el_id('id-ext-data-container');
+   if (!height) {
+      el.style.height = '';      // revert to default
+   } else {
+      el.style.height = px(height);
+   }
+}
+
 function ext_set_controls_width_height(width, height)
 {
 	panel_set_width_height('ext-controls', width, height);
@@ -340,6 +350,7 @@ function extint_select_menu()
 		if (!dbgUs && extint_names[i] == 's4285') continue;	// FIXME: hide while we develop
 		if (!dbgUs && extint_names[i] == 'test') continue;	// FIXME: hide while we develop
 		if (!dbgUs && extint_names[i] == 'timecode') continue;	// FIXME: hide while we develop
+		if (!dbgUs && extint_names[i] == 'fax') continue;	// FIXME: hide while we develop
 		s += '<option value="'+ i +'">'+ extint_names[i] +'</option>';
 	}
 	//console.log('extint_select_menu = '+ s);
