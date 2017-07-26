6f19e4d3e8260e9166aea924d71e2682ed2e8544
jks-prv <jks@jks.com>
2017-07-26 10:35:12 -1000

    create .xz compressed sample files

--- tools/wspr.c
+++ tools/wspr.c
@@ -36,30 +36,47 @@ This produces a file full of zeros!
 cat wspr.fs375.cf7040100.iq.s16.dat | csdr convert_s16_f | csdr fir_interpolate_cc 32 0.001 > wspr.fs12k.cf7040100.iq.f.dat
 
 This works, but there is unexpected data at the EOF we chop off.
-There is still a period of silence at the EOF.
-cat wspr.fs375.cf7040100.iq.s16.dat | csdr convert_s16_f | csdr plain_interpolate_cc 32 | csdr bandpass_fir_fft_cc -0.015625 0.015625 0.001 | dd bs=8 count=1440000 > wspr.fs12k.cf7040100.iq.f.dat
+There is still a period of silence at the EOF unless we append extra samples below to cover the pipeline gap of the interpolator.
+cat wspr.fs375.cf7040100.iq.s16.dat | csdr convert_s16_f | csdr plain_interpolate_cc 32 | csdr bandpass_fir_fft_cc -0.015625 0.015625 0.002 | dd bs=8 count=1440000 > wspr.fs12k.cf7040100.iq.f.dat
 */
+
+#define GAIN 300
+#define MIN_MAX 0
 
 int main()
 {
     int i, j, fd;
+    TYPECPX *iq;
+    short re, im;
     
     scall("open", (fd = open("wspr.fs375.cf7040100.iq.s16.dat", O_WRONLY | O_CREAT, 0666)));
     
     TYPEREAL max = 0, min = 0;
+
     for (i = 0; i < WSPR_DEMO_NSAMPS; i++) {
-        TYPECPX *iq = &wspr_demo_samps[i];
-        if (iq->re > max) max = iq->re;
-        if (iq->re < min) min = iq->re;
-        if (iq->im > max) max = iq->im;
-        if (iq->im < min) min = iq->im;
-        #define GAIN 300
-        short re = iq->re * GAIN;
-        short im = iq->im * GAIN;
+        iq = &wspr_demo_samps[i];
+        #if MIN_MAX
+            if (iq->re > max) max = iq->re;
+            if (iq->re < min) min = iq->re;
+            if (iq->im > max) max = iq->im;
+            if (iq->im < min) min = iq->im;
+        #endif
+        re = iq->re * GAIN;
+        im = iq->im * GAIN;
         write(fd, &re, 2);
         write(fd, &im, 2);
     }
-    printf("max %.3f min %.3f\n", max, min);
+
+    // append some extra samples to prevent interpolator pipeline gap
+    for (i = 0; i < WSPR_DEMO_NSAMPS/10; i++) {
+        iq = &wspr_demo_samps[i];
+        re = iq->re * GAIN;
+        im = iq->im * GAIN;
+        write(fd, &re, 2);
+        write(fd, &im, 2);
+    }
+
+    if (MIN_MAX) printf("max %.3f min %.3f\n", max, min);
     close(fd);
     
 	return 0;
