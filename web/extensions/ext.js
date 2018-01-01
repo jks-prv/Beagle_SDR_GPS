c8215e174c80bfcd5c2acb9639007cfab8fdeb81
jks-prv <jks@jks.com>
2018-01-02 12:19:59 +1300

    hide colormap ext for now

--- web/extensions/ext.js
+++ web/extensions/ext.js
@@ -411,6 +411,7 @@ function extint_select_menu()
 		if (!dbgUs && extint_names[i] == 's4285') continue;	// FIXME: hide while we develop
 		if (!dbgUs && extint_names[i] == 'test') continue;	// FIXME: hide while we develop
 		if (!dbgUs && extint_names[i] == 'timecode') continue;	// FIXME: hide while we develop
+		if (!dbgUs && extint_names[i] == 'colormap') continue;	// FIXME: hide while we develop
 
 		s += '<option value="'+ i +'">'+ extint_names[i] +'</option>';
 	}
