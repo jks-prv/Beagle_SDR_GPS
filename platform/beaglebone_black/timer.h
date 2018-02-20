76ba06f0ecfa4b5dee4aa8dfebaa2ee74e727245
jks-prv <jks@jks.com>
2018-02-20 20:05:58 +1300

    fix due to change in our DEVSYS tools

--- platform/beaglebone_black/timer.h
+++ platform/beaglebone_black/timer.h
@@ -3,12 +3,14 @@
 #include "types.h"
 
 // not all development systems have clock_gettime()
-//#ifdef DEVSYS
-//	#define clock_gettime(clk_id, tp)
-//	#define CLOCK_MONOTONIC 0
-//#else
+#ifdef DEVSYS
+	#define clock_gettime(clk_id, tp)
+	#define clock_settime(clk_id, tp) 0
+	#define CLOCK_MONOTONIC 0
+	#define CLOCK_REALTIME 0
+#else
 	#include <time.h>
-//#endif
+#endif
 
 #define CTIME_R_BUFSIZE     (25 + SPACE_FOR_NULL)
 #define CTIME_R_NL          24      // offset of '\n' in ctime_r return buffer
