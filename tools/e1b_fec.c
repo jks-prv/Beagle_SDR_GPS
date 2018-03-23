01f9aad58d1f31101dc94352b027a604b506374d
jks-prv <jks@jks.com>
2018-03-24 07:05:18 +1300

    tool to test Viterbi decoding

---
+++ tools/e1b_fec.c
@@ -0,0 +1,172 @@
+#include "types.h"
+#include "fec.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdlib.h>
+
+extern int encode(
+unsigned char *symbols,
+unsigned char *data,
+unsigned int nbytes,
+unsigned int startstate,
+unsigned int endstate);
+
+extern int init_viterbi();
+extern int viterbi(char *logfile, unsigned char *ibuf, int ilen, unsigned char *obuf, int *olen);
+
+int main()
+{
+    int i, j;
+    
+    const int input[121] = {
+        1,1,1,1,1,1,1,1, 1,1,1,1,0,0,0,0, 1,1,0,0,1,1,0,0, 1,0,1,0,1,0,1,0, 0,0,0,0,0,0,0,0, 0,0,0,0,1,1,1,1,
+        0,0,1,1,0,0,1,1, 0,1,0,1,0,1,0,1, 1,1,1,0,0,0,1,1, 1,1,1,0,1,1,0,0, 1,1,0,1,1,1,1,1, 1,0,0,0,1,0,1,0,
+        0,0,0,1,1,1,0,0, 0,0,0,1,0,0,1,1, 0,1,0,0,0,0,0,0,
+        -1
+    };
+    
+    int out[240];
+    int op=0;
+    
+    int q[6];
+    for (j=0; j<6; j++) q[j] = 0;
+    
+    // [2,1,7] CC(171, 133-inverted)
+    #define G2_INVERTED 1
+    int in;
+    for (i=0; (in = input[i]) != -1; i++) {
+        u1_t g1 = in ^ q[0] ^ q[1] ^ q[2] ^ q[5];
+        u1_t g2 = in ^ q[1] ^ q[2] ^ q[4] ^ q[5] ^ G2_INVERTED;
+        //printf("%3d: ", i);
+        //for (j=0; j<6; j++) printf("%d", q[j]);
+        //printf(" g1=%d g2=%d\n", g1, g2);
+        out[op++] = g1;
+        out[op++] = g2;
+        
+        for (j=5; j>0; j--) {
+            q[j] = q[j-1];
+        }
+        q[0] = in;
+    }
+    //printf("\n");
+    
+    printf("our encode:\n");
+    for (i=0; i<240; i++) {
+        printf("%d", out[i]);
+        if ((i%8) == 7) printf(" ");
+    }
+    printf("\n");
+    
+    ////////////////////////////////
+    
+    u1_t data[120] = {
+        1,1,1,1,1,1,1,1, 1,1,1,1,0,0,0,0, 1,1,0,0,1,1,0,0, 1,0,1,0,1,0,1,0, 0,0,0,0,0,0,0,0, 0,0,0,0,1,1,1,1,
+        0,0,1,1,0,0,1,1, 0,1,0,1,0,1,0,1, 1,1,1,0,0,0,1,1, 1,1,1,0,1,1,0,0, 1,1,0,1,1,1,1,1, 1,0,0,0,1,0,1,0,
+        0,0,0,1,1,1,0,0, 0,0,0,1,0,0,1,1, 0,1,0,0,0,0,0,0,
+    };
+    
+    u1_t syms[240+12];
+    
+    #if 1
+        printf("viterbi.c encode:\n");
+        encode(syms, data, 120/8, 0, 0);
+    #else
+        printf("copy of our encode:\n");
+        for (i=0; i<(240+12); i++) syms[i] = out[i];
+    #endif
+    
+    for (i=0; i<240; i++) {
+        printf("%d", syms[i]);
+        if ((i%8) == 7) printf(" ");
+    }
+    printf("\n");
+    
+    printf("diffs:\n");
+    for (i=0; i<240; i++) {
+        printf("%c", (out[i] != syms[i])? 'X':'.');
+        if ((i%8) == 7) printf(" ");
+    }
+    printf("\n");
+    
+    printf("\ninput:\n");
+    for (i=0; i<120; i++) {
+        printf("%d", data[i]);
+        if ((i%8) == 7) printf(" ");
+    }
+    printf("\n\n");
+
+    u1_t deco[120];
+    for (i=0; i<120; i++) deco[i] = 8;
+    int decolen;
+    u1_t deco8[120/8];
+    
+    printf("viterbi.c decode:\n");
+    init_viterbi();
+    viterbi(NULL, syms, (120+6)*2, deco, &decolen);
+    //printf("decolen=%d\n", decolen);
+    for (i=0; i<120; i++) {
+        printf("%d", deco[i]);
+        if ((i%8) == 7) printf(" ");
+    }
+    printf("\n");
+
+    printf("diffs:\n");
+    for (i=0; i<120; i++) {
+        printf("%c", (data[i] != deco[i])? 'X':'.');
+        if ((i%8) == 7) printf(" ");
+    }
+    printf("\n");
+    
+    printf("\nviterbi27.c decode:\n");
+    int polys[2] = { 0x4f, -0x6d };         // k=7; Galileo E1B; "-" means G2 inverted
+    set_viterbi27_polynomial_port(polys);
+    int framebits = 120;
+    void *v27 = create_viterbi27_port(/*len*/ framebits);
+    for (i=0; i<240; i++) syms[i] = syms[i]? 255:0;
+    update_viterbi27_blk_port(v27, syms, /*npairs*/ framebits+6);
+    chainback_viterbi27_port(v27, deco8, /*nbits*/ framebits, /*endstate*/ 0);
+    for (i=0; i<120/8; i++) {
+        u1_t b8 = deco8[i];
+        for (j=0; j<8; j++) {
+            u1_t b1 = (b8 >> (7-j)) & 1;
+            printf("%d", b1);
+            deco[i*8+j] = b1;
+        }
+        printf(" ");
+    }
+    printf("\n");
+    
+    printf("diffs:\n");
+    for (i=0; i<120; i++) {
+        printf("%c", (data[i] != deco[i])? 'X':'.');
+        if ((i%8) == 7) printf(" ");
+    }
+    printf("\n");
+    
+    exit(0);
+    
+    // block interleaver: 30 cols (r++), 8 rows (w++)
+    printf("\n\nencoded & interleaved:\n");
+    int blk[8][30];
+    op=0;
+    for (int c=0; c < 30; c++) {
+        for (int r=0; r < 8; r++) {
+            blk[r][c] = out[op++];
+        }
+    }
+    
+    op=0;
+    for (int r=0; r < 8; r++) {
+        for (int c=0; c < 30; c++) {
+            printf("%d", blk[r][c]);
+            if ((op%8) == 7) printf(" ");
+            op++;
+        }
+    }
+    printf("\n");
+    
+	return 0;
+}
---
+++ tools/viterbi27_test.c
@@ -0,0 +1,215 @@
+/* Test viterbi decoder speeds */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <math.h>
+#include <memory.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#ifdef HAVE_GETOPT_H
+#include <getopt.h>
+#endif
+#include "fec.h"
+
+#define	MAX_RANDOM	0x7fffffff
+
+double normal_rand(){
+  const double k1 = sqrt(2/M_E);
+  const double s = 0.449871;
+  const double a = 0.19600;
+  const double t = -0.386595;
+  const double b = 0.25472;
+  const double r1 = 0.27597;
+  const double r2 = 0.27846;
+  double u,v,x,y,Q;
+
+  while(1){
+    do {
+      u = (double)random() / MAX_RANDOM;
+    } while (u == 0.);
+    v = k1 * 2.0 * (double)random() / MAX_RANDOM - 1;
+    x = u - s;
+    y = fabs(v) - t;
+    Q = x*x + y*(a*y - b*x);
+    if(Q < r1)
+      return v/u;
+
+    if(Q < r2 && v*v < -4*u*u*log(u))
+	return v/u;
+  }
+}
+
+void addnoise(unsigned char *symbols,int length,int ampl,double noise){
+  float r;
+  unsigned char c;
+
+  while(length-- > 0){
+    r = normal_rand();
+//jks
+noise=0;
+printf("%d ", *symbols);
+    r = noise * r + 127.5 + (*symbols ? ampl : -ampl);
+
+    c = r > 255 ? 255 : (r < 0 ? 0 : r);
+    *symbols++ = c;
+  }
+}
+
+#if HAVE_GETOPT_LONG
+struct option Options[] = {
+  {"frame-length",1,NULL,'l'},
+  {"frame-count",1,NULL,'n'},
+  {"ebn0",1,NULL,'e'},
+  {"gain",1,NULL,'g'},
+  {"verbose",0,NULL,'v'},
+  {NULL},
+};
+#endif
+
+#define RATE (1./2.)
+#define MAXBYTES 10000
+
+double Gain = 100.0;
+int Verbose = 0;
+
+int main(int argc,char *argv[]){
+  int i,d,tr;
+  int sr=0,trials = 10000,errcnt,framebits=2048;
+  long long int tot_errs=0;
+  unsigned char bits[MAXBYTES];
+  unsigned char data[MAXBYTES];
+  unsigned char xordata[MAXBYTES];
+  unsigned char symbols[8*2*(MAXBYTES+6)];
+  void *vp;
+  extern char *optarg;
+  struct rusage start,finish;
+  double extime;
+  double noise,esn0,ebn0;
+  time_t t;
+  int badframes=0;
+
+  time(&t);
+  srandom(t);
+  ebn0 = -100;
+
+#if HAVE_GETOPT_LONG
+  while((d = getopt_long(argc,argv,"l:n:e:g:v",Options,NULL)) != EOF){
+#else
+  while((d = getopt(argc,argv,"l:n:e:g:v")) != EOF){
+#endif
+    switch(d){
+    case 'l':
+      framebits = atoi(optarg);
+      break;
+    case 'n':
+      trials = atoi(optarg);
+      break;
+    case 'e':
+      ebn0 = atof(optarg);
+      break;
+    case 'g':
+      Gain = atof(optarg);
+      break;
+    case 'v':
+      Verbose++;
+      break;
+    }
+  }
+  if(framebits > 8*MAXBYTES){
+    fprintf(stderr,"Frame limited to %d bits\n",MAXBYTES*8);
+    framebits = MAXBYTES*8;
+  }
+  if((vp = create_viterbi27(framebits)) == NULL){
+    printf("create_viterbi27 failed\n");
+    exit(1);
+  }
+  if(ebn0 != -100){
+    esn0 = ebn0 + 10*log10((double)RATE); /* Es/No in dB */
+    /* Compute noise voltage. The 0.5 factor accounts for BPSK seeing
+     * only half the noise power, and the sqrt() converts power to
+     * voltage.
+     */
+    noise = Gain * sqrt(0.5/pow(10.,esn0/10.));
+    
+    printf("Viterbi27 test: nframes = %d framesize = %d ebn0 = %.2f dB gain = %g noise = %g\n",trials,framebits,ebn0,Gain,noise);
+    
+    for(tr=0;tr<trials;tr++){
+      /* Encode a frame of random data */
+      for(i=0;i<framebits+6;i++){
+	int bit = (i < framebits) ? (random() & 1) : 0;
+	
+	sr = (sr << 1) | bit;
+	bits[i/8] = sr & 0xff;
+	symbols[2*i+0] = parity(sr & V27POLYA);
+	symbols[2*i+1] = parity(sr & V27POLYB);
+      }	
+      addnoise(symbols,2*(framebits+6),Gain,noise);
+      /* Decode it and make sure we get the right answer */
+      /* Initialize Viterbi decoder */
+      init_viterbi27(vp,0);
+      
+      /* Decode block */
+for(i=0;i<framebits+6;i++) printf("%d %d ", symbols[2*i+0], symbols[2*i+1]);
+printf("\n");
+      update_viterbi27_blk(vp,symbols,framebits+6);
+      
+      /* Do Viterbi chainback */
+for(i=0;i<framebits/8;i++) data[i] = 0xaa;
+      chainback_viterbi27(vp,data,framebits,0);
+for(i=0;i<framebits/8;i++) printf("0x%08x ", data[i]);
+printf("\n");
+      errcnt = 0;
+      for(i=0;i<framebits/8;i++){
+	int e = popcnt(xordata[i] = data[i] ^ bits[i]);
+	errcnt += e;
+	printf("trial %d framebits %d errors %d\n", tr, framebits, e);
+	tot_errs += e;
+      }
+      if(errcnt != 0)
+	badframes++;
+      if(Verbose > 1 && errcnt != 0){
+	printf("frame %d, %d errors: ",tr,errcnt);
+	for(i=0;i<framebits/8;i++){
+	  printf("%02x",xordata[i]);
+	}
+	printf("\n");
+      }
+      if(Verbose)
+	printf("BER %lld/%lld (%10.3g) FER %d/%d (%10.3g)\r",
+	       tot_errs,(long long)framebits*(tr+1),tot_errs/((double)framebits*(tr+1)),
+	       badframes,tr+1,(double)badframes/(tr+1));
+      fflush(stdout);
+    }
+    if(Verbose > 1)
+      printf("nframes = %d framesize = %d ebn0 = %.2f dB gain = %g\n",trials,framebits,ebn0,Gain);
+    else if(Verbose == 0)
+      printf("BER %lld/%lld (%.3g) FER %d/%d (%.3g)\n",
+	     tot_errs,(long long)framebits*trials,tot_errs/((double)framebits*trials),
+	     badframes,tr+1,(double)badframes/(tr+1));
+    else
+      printf("\n");
+
+  } else {
+    /* Do time trials */
+    memset(symbols,127,sizeof(symbols));
+    printf("Starting time trials\n");
+    getrusage(RUSAGE_SELF,&start);
+    for(tr=0;tr < trials;tr++){
+      /* Initialize Viterbi decoder */
+      init_viterbi27(vp,0);
+      
+      /* Decode block */
+      update_viterbi27_blk(vp,symbols,framebits);
+      
+      /* Do Viterbi chainback */
+      chainback_viterbi27(vp,data,framebits,0);
+    }
+    getrusage(RUSAGE_SELF,&finish);
+    extime = finish.ru_utime.tv_sec - start.ru_utime.tv_sec + 1e-6*(finish.ru_utime.tv_usec - start.ru_utime.tv_usec);
+    printf("Viterbi27 execution time for %d %d-bit frames: %.2f sec\n",trials,
+	   framebits,extime);
+    printf("decoder speed: %g bits/s\n",trials*framebits/extime);
+  }
+  exit(0);
+}
