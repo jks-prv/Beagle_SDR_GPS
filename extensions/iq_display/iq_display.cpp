a7e3426ee061fb10fbc6ecc74cd811378d2d3a00
jks-prv <jks@jks.com>
2018-05-05 15:25:09 +1200

    UI for Christoph's IQ display mPSK support

--- extensions/iq_display/iq_display.cpp
+++ extensions/iq_display/iq_display.cpp
@@ -1,9 +1,9 @@
 // Copyright (c) 2016 John Seamons, ZL/KF6VO
 
-#include "ext.h"	// all calls to the extension interface begin with "ext_", e.g. ext_register()
+#include "ext.h"    // all calls to the extension interface begin with "ext_", e.g. ext_register()
 
 #ifndef EXT_IQ_DISPLAY
-	void iq_display_main() {}
+    void iq_display_main() {}
 #else
 
 #include "clk.h"
@@ -17,68 +17,81 @@
 #include <math.h>
 #include <strings.h>
 
-//#define IQ_DISPLAY_DEBUG_MSG	true
-#define IQ_DISPLAY_DEBUG_MSG	false
+//#define IQ_DISPLAY_DEBUG_MSG  true
+#define IQ_DISPLAY_DEBUG_MSG    false
 
 // rx_chan is the receiver channel number we've been assigned, 0..RX_CHAN
 // We need this so the extension can support multiple users, each with their own iq_display[] data structure.
 
-#define	IQ_POINTS		0
-#define	IQ_DENSITY		1
-// #define	IQ_S4285_P		2
-// #define	IQ_S4285_D		3
-// #define	IQ_CLEAR		4
+#define IQ_POINTS       0
+#define IQ_DENSITY      1
+// #define  IQ_S4285_P      2
+// #define  IQ_S4285_D      3
+// #define  IQ_CLEAR        4
 
 #define N_IQ_RING (16*1024)
-#define N_CH		2
-#define N_HISTORY	2
+#define N_CH        2
+#define N_HISTORY   2
 
 #include <array>
 #include <complex>
 #include <memory>
-
-#include "iir.hpp"
-#include "pll.hpp"
 
 class iq_display {
 public:
     typedef std::unique_ptr<iq_display> sptr;
     ~iq_display() {}
 
-    typedef filter::loop_filter_2nd<float > loop_filter_type;
-    typedef filter::integrator_modulo<float > integrator_type;
-    typedef filter::pll<float, loop_filter_type, integrator_type> pll_type;
-
     static sptr make(int rx_chan) {
         return sptr(new iq_display(rx_chan));
     }
-    std::complex<u1_t> to_fixed_scale(int ch, std::complex<float> sample) const {
-        sample *= _gain * (ch ? 20 : 1) * 255 / CUTESDR_MAX_VAL;
+    std::complex<u1_t> to_u1(int ch, std::complex<float> sample) const {
+        const float scale_factor = 255*(_gain ? _gain * (ch ? 20 : 1) / CUTESDR_MAX_VAL : 1.0f/(4.0*_ama));
+        sample *= scale_factor;
         return std::complex<u1_t>(u1_t(std::max(0.0f, std::min(255.0f, 127+sample.real()))),
                                   u1_t(std::max(0.0f, std::min(255.0f, 127+sample.imag()))));
     }
-    std::complex<u1_t> to_auto_scale(int ch, std::complex<float> sample) const {
-        sample *= 255 / (4.0*std::abs(_cma));
-        return std::complex<u1_t>(u1_t(std::max(0.0f, std::min(255.0f, 127+sample.real()))),
-                                  u1_t(std::max(0.0f, std::min(255.0f, 127+sample.imag()))));
+    std::complex<float> process_sample(TYPECPX *s) {
+        std::complex<float> sample(s->re, s->im);
+        
+        if (_exponent) {
+            std::complex<float> exp_sample = (std::complex<float>) std::pow(sample, _exponent);
+    
+            // update pll
+            _phase = std::fmod(_phase+_df/_fs, float(8*2*M_PI));
+            const float ud_old = _ud;
+            _ud  = std::arg(exp_sample * std::exp(std::complex<float>(0.0f, -_phase)));
+            _df +=_b[0]*_ud + _b[1]*ud_old;
+    
+            std::complex<float> recovered_carrier = std::exp(std::complex<float>(0.0f, -_phase/_exponent));
+            if (_mode == 0) {
+                /*
+                float sI = real(sample);
+                float sQ = imag(sample);
+                float Csin = imag(recovered_carrier);
+                float Ccos = real(recovered_carrier);
+                sample = std::complex<float>(Ccos * sI - Csin * sQ, Csin * sI + Ccos * sQ);
+                */
+                sample *= recovered_carrier;
+            } else {
+                sample = recovered_carrier;
+            }
+        }
+
+        _cma    = (float(_maN)*_cma +          sample )/float(_maN+1);
+        _ama    = (float(_maN)*_ama + std::abs(sample))/float(_maN+1);
+        _maN  += (_maN < int(0.5*_fs));
+        return sample;
     }
+    
     void display_data(int ch, int nsamps, TYPECPX *samps) {
         const int cmd = (_draw<<1) + (ch&1);
         if (_draw == IQ_POINTS) {
             for (int i=0; i<nsamps; ++i) {
                 const std::complex<float> old_sample(_iq[ch][_ring[ch]]);
-                std::complex<float> new_sample(samps[i].re, samps[i].im);
-                _pll.process(new_sample);
-                new_sample *= std::conj(_pll.exp_i_phase());
-                _cma = (float(_cmaN)*_cma + new_sample)/float(_cmaN+1);
-                _cmaN += (_cmaN < int(0.5*_fs));
-                if (_gain) { // fixed scale
-                    _plot[ch][_ring[ch]][0] = to_fixed_scale(ch, old_sample);
-                    _plot[ch][_ring[ch]][1] = to_fixed_scale(ch, new_sample);
-                } else { // auto scale
-                    _plot[ch][_ring[ch]][0] = to_auto_scale(ch, old_sample);
-                    _plot[ch][_ring[ch]][1] = to_auto_scale(ch, new_sample);
-                }
+                const std::complex<float> new_sample = process_sample(samps+i);
+                _plot[ch][_ring[ch]][0] = to_u1(ch, old_sample);
+                _plot[ch][_ring[ch]][1] = to_u1(ch, new_sample);
                 _iq[ch][_ring[ch]] = new_sample;
                 _ring[ch] += 1;
                 if (_ring[ch] >= _points) {
@@ -89,16 +102,8 @@ public:
         }
         if (_draw == IQ_DENSITY) {
             for (int i=0; i<nsamps; ++i) {
-                std::complex<float> new_sample(samps[i].re, samps[i].im);
-                _pll.process(new_sample);
-                new_sample *= std::conj(_pll.exp_i_phase());
-                _cma = (float(_cmaN)*_cma + new_sample)/float(_cmaN+1);
-                _cmaN += (_cmaN < int(0.5*_fs));
-                if (_gain) {
-                    _map[_ring[ch]] = to_fixed_scale(ch, new_sample);
-                } else {
-                    _map[_ring[ch]] = to_auto_scale(ch, new_sample);
-                }
+                const std::complex<float> new_sample = process_sample(samps+i);
+                _map[_ring[ch]] = to_u1(ch, new_sample);
                 _ring[ch] += 1;
                 if (_ring[ch] >= _points) {
                     ext_send_msg_data(_rx_chan, IQ_DISPLAY_DEBUG_MSG, cmd, (u1_t*)(&(_map[0])), 1 + _points*2);
@@ -107,31 +112,43 @@ public:
             }
         }
         _nsamp += nsamps;
-        if (_nsamp > _cmaNsend) {
-            ext_send_msg(_rx_chan, IQ_DISPLAY_DEBUG_MSG, "EXT cmaI=%e cmaQ=%e df=%e", _cma.real(), _cma.imag(), -_pll.uf()/(2*M_PI));
-            _nsamp -= _cmaNsend;
+        if (_nsamp > _maNsend) {
+            float df = _exponent? _df/(2*M_PI*_exponent) : 0;
+            ext_send_msg(_rx_chan, IQ_DISPLAY_DEBUG_MSG, "EXT cmaI=%e cmaQ=%e df=%e", _cma.real(), _cma.imag(), df);
+            _nsamp -= _maNsend;
         }
-}
+    }
 
     void set_sample_rate(float fs) {
         _fs       = fs;
-        _cmaNsend = fs/4;
-        _pll.update_ppb(0.0, fs);
+        _maNsend = fs/4;
+        pll_init();
     }
 
     bool process_msg(const char* msg) {
         int gain = 0;
         if (sscanf(msg, "SET gain=%d", &gain) == 1)
-            set_gain(gain? pow(10.0, ((float) gain - 50) / 10.0) : 0); // 0 .. +100 dB of CUTESDR_MAX_VAL
+            set_gain(gain? std::pow(10.0, ((float) gain - 50) / 10.0) : 0); // 0 .. +100 dB of CUTESDR_MAX_VAL
 
         int points = 0;
         if (sscanf(msg, "SET points=%d", &points) == 1)
             set_points(points);
 
-	
+        int exponent = 0;
+        if (sscanf(msg, "SET exponent=%d", &exponent) == 1) // allowed values are 1,2,4,8
+            set_exponent(exponent);
+
+        float pll_bandwidth = 0;
+        if (sscanf(msg, "SET pll_bandwidth=%f", &pll_bandwidth) == 1)
+            set_pll_bandwidth(pll_bandwidth);
+
         int draw = 0;
         if (sscanf(msg, "SET draw=%d", &draw)== 1)
             set_draw(draw);
+
+        int mode = 0;
+        if (sscanf(msg, "SET mode=%d", &mode)== 1)
+            set_mode(mode);
 
         if (strcmp(msg, "SET clear") == 0)
             clear();
@@ -142,52 +159,76 @@ public:
 protected:
     int rx_chan() const { return _rx_chan; }
 
-    void set_gain(float g) { _gain = g; }
-    void set_points(int p) { _points = p; }
-    void set_draw(int d) { _draw = d; }
+    void set_gain(float arg) { _gain = arg; }
+    void set_points(int arg) { _points = arg; }
+    void set_exponent(int arg) { _exponent = arg; }
+    void set_pll_bandwidth(float arg) { _pll_bandwidth = arg; }
+    void set_draw(int arg) { _draw = arg; }
+    void set_mode(int arg) { _mode = arg; }
 
     void clear() {
-        //printf("cmaN %d cmaI %e cmaQ %e\n", e->cmaN, e->cmaI, e->cmaQ);
-        //		e->cma = e->ncma = e->cmaI = e->cmaQ = e->cmaN = e->nsamp = 0;
+        //printf("maN %d cmaI %e cmaQ %e\n", e->maN, e->cmaI, e->cmaQ);
+        //      e->cma = e->ncma = e->cmaI = e->cmaQ = e->maN = e->nsamp = 0;
+        _ama   = 0;
         _cma   = 0;
-        _cmaN  = 0;
+        _maN  = 0;
         _nsamp = 0;
-        _pll.reset();
+        pll_init();
     }
-    
+
 private:
+    void pll_init() {
+        const float xi  = 1/sqrt(2.0f);         // PLL damping
+        const float dwl = _exponent*_pll_bandwidth; // PLL bandwidth in Hz
+        const float wn  = M_PI*dwl/xi;
+        const float tau[2] = { 1/(wn*wn), 2*xi/wn };
+        const float ts2 = 0.5/_fs;
+        _b[0] = ts2/tau[0]*(1 + 1/std::tan(ts2/tau[1]));
+        _b[1] = ts2/tau[0]*(1 - 1/std::tan(ts2/tau[1]));
+        _phase = _ud = 0;
+    }
     iq_display(int rx_chan)
         : _rx_chan(rx_chan)
         , _points(1024)
         , _nsamp(0)
+        , _exponent(1)
         , _draw(0)
+        , _mode(0)
         , _gain(0.0f)
         , _fs(12000.0f)
-        , _cmaN(0)
+        , _maN(0)
         , _cma(0,0)
-        , _cmaNsend(0)
-        , _loop_filter(1.0f/sqrt(2.0f), 20.0f, 12001.0f)
-        , _integrator(2*M_PI)
-        , _pll(0.0f, 12001.0f, 20.0f, _loop_filter, _integrator) { _ring[0] = _ring[1] = 0; }
+        , _ama(0)
+        , _maNsend(0)
+        , _pll_bandwidth(10) {
+        _ring[0] = _ring[1] = 0;
+        pll_init();
+    }
     iq_display(const iq_display&);
     iq_display& operator=(const iq_display&);
 
     int   _rx_chan;
     int   _points;
     int   _nsamp;
+    int   _exponent;
     int   _draw;
+    int   _mode;
     float _gain;
     float _fs; // sample rate
     int   _ring[N_CH];
     std::complex<float>  _iq[N_CH][N_IQ_RING];
     std::complex<u1_t> _plot[N_CH][N_IQ_RING][N_HISTORY];
     std::complex<u1_t>  _map[N_IQ_RING];
-    int                 _cmaN; // for moving average
-    std::complex<float> _cma;  // moving average
-    int                 _cmaNsend; // for cma
-    loop_filter_type _loop_filter;
-    integrator_type  _integrator;
-    pll_type         _pll;
+    int                 _maN; // for moving average
+    std::complex<float> _cma;  // complex moving average
+    float               _ama;  // moving average of abs(z)
+    int                 _maNsend; // for cma
+    // PLL
+    float _pll_bandwidth;  // PLL bandwidth in Hz
+    float _df;         // frequency offset in radians per sample
+    float _phase;      // accumulated phase for frequency correction
+    float _b[2];       // loop filter coefficients
+    float _ud;         //
 } ;
 
 std::array<iq_display::sptr, RX_CHANS> iqs;
@@ -204,50 +245,50 @@ void iq_display_close(int rx_chan) {
 
 bool iq_display_msgs(char *msg, int rx_chan)
 {
-	if (strcmp(msg, "SET ext_server_init") == 0) {
+    if (strcmp(msg, "SET ext_server_init") == 0) {
         iqs[rx_chan] = iq_display::make(rx_chan);
-		ext_send_msg(rx_chan, IQ_DISPLAY_DEBUG_MSG, "EXT ready");
-		return true;
-	}
+        ext_send_msg(rx_chan, IQ_DISPLAY_DEBUG_MSG, "EXT ready");
+        return true;
+    }
 
     int do_run = 0;
-	if (sscanf(msg, "SET run=%d", &do_run)) {
-		if (do_run) {
+    if (sscanf(msg, "SET run=%d", &do_run)) {
+        if (do_run) {
             iqs[rx_chan]->set_sample_rate(ext_update_get_sample_rateHz(rx_chan));
-			ext_register_receive_iq_samps(iq_display_data, rx_chan);
-		} else {
-			ext_unregister_receive_iq_samps(rx_chan);
-		}
-		return true;
-	}
+            ext_register_receive_iq_samps(iq_display_data, rx_chan);
+        } else {
+            ext_unregister_receive_iq_samps(rx_chan);
+        }
+        return true;
+    }
 
-	// SECURITY
-	// FIXME: need a per-user PLL instead of just changing the clock offset
-	float offset = 0.0f;
-	if (sscanf(msg, "SET offset=%f", &offset) == 1) {
-		ext_adjust_clock_offset(rx_chan, offset);
-		return true;
-	}
+    // SECURITY
+    // FIXME: need a per-user PLL instead of just changing the clock offset
+    float offset = 0.0f;
+    if (sscanf(msg, "SET offset=%f", &offset) == 1) {
+        ext_adjust_clock_offset(rx_chan, offset);
+        return true;
+    }
 
     if (iqs[rx_chan])
         return iqs[rx_chan]->process_msg(msg);
-    	
-	return false;
+
+    return false;
 }
 
 
 void iq_display_main();
 
 ext_t iq_display_ext = {
-	"iq_display",
-	iq_display_main,
-	iq_display_close,
-	iq_display_msgs,
+    "iq_display",
+    iq_display_main,
+    iq_display_close,
+    iq_display_msgs,
 };
 
 void iq_display_main()
 {
-	ext_register(&iq_display_ext);
+    ext_register(&iq_display_ext);
 }
 
 #endif
--- web/extensions/iq_display/iq_display.js
+++ web/extensions/iq_display/iq_display.js
@@ -2,6 +2,17 @@
 
 var iq_display_ext_name = 'iq_display';		// NB: must match iq_display.c:iq_display_ext.name
 
+var iq = {
+   cmd_e: { IQ_POINTS:0, IQ_DENSITY:1, IQ_S4285_P:2, IQ_S4285_D:3, IQ_CLEAR:4 },
+   draw: 0,
+   mode: 0,
+   cmaI: 0,
+   cmaQ: 0,
+   df: 0,
+   pll: 1,
+   pll_bw: 10,
+};
+
 var iq_display_first_time = true;
 
 function iq_display_main()
@@ -32,8 +43,6 @@ function iq_display_clear()
 }
 
 var iq_display_imageData;
-var iq_display_draw;
-var iq_display_cmaI, iq_display_cmaQ;
 var iq_display_upd_cnt = 0;
 
 function iq_display_update()
@@ -41,7 +50,7 @@ function iq_display_update()
 	//console.log('iq_display_update '+ iq_display_max);
 	var c = iq_display_canvas.ctx;
 
-	if (iq_display_draw == iq_display_cmd_e.IQ_DENSITY || iq_display_draw == iq_display_cmd_e.IQ_S4285_D) {
+	if (iq.draw == iq.cmd_e.IQ_DENSITY || iq.draw == iq.cmd_e.IQ_S4285_D) {
 		var y=0;
 		for (var q=0; q < (256*256); q += 256) {
 			for (var i=0; i < 256; i++) {
@@ -58,7 +67,7 @@ function iq_display_update()
 	
 	if (iq_display_upd_cnt == 3) {
       w3_el('iq_display-cma').innerHTML =
-         'CMA I: '+ iq_display_cmaI.toExponential(2) +'&nbsp; &nbsp; CMA Q: '+ iq_display_cmaQ.toExponential(2);
+         'I='+ iq.cmaI.toExponential(1).withSign() +' Q='+ iq.cmaQ.toExponential(1).withSign() +' df='+ iq.df.toExponential(1).withSign();
       w3_el('iq_display-adc').innerHTML =
          'ADC clock: '+ (ext_adc_clock_Hz()/1e6).toFixed(6) +' MHz';
       w3_el('iq_display-gps').innerHTML =
@@ -67,8 +76,6 @@ function iq_display_update()
    }
    iq_display_upd_cnt++;
 }
-
-var iq_display_cmd_e = { IQ_POINTS:0, IQ_DENSITY:1, IQ_S4285_P:2, IQ_S4285_D:3, IQ_CLEAR:4 };
 
 function iq_display_recv(data)
 {
@@ -81,7 +88,7 @@ function iq_display_recv(data)
 		var ch = ba[0] & 1;
 		var len = ba.length-1;
 
-		if (cmd == iq_display_cmd_e.IQ_POINTS || cmd == iq_display_cmd_e.IQ_S4285_P) {
+		if (cmd == iq.cmd_e.IQ_POINTS || cmd == iq.cmd_e.IQ_S4285_P) {
 			var c = iq_display_canvas.ctx;
 			var i, q;
 
@@ -98,7 +105,7 @@ function iq_display_recv(data)
 			}
 		} else
 		
-		if (cmd == iq_display_cmd_e.IQ_DENSITY || cmd == iq_display_cmd_e.IQ_S4285_D) {
+		if (cmd == iq.cmd_e.IQ_DENSITY || cmd == iq.cmd_e.IQ_S4285_D) {
 			//console.log('IQ_DENSITY '+ len);
 			var c = iq_display_canvas.ctx;
 			var i, q;
@@ -113,7 +120,7 @@ function iq_display_recv(data)
 			}
 		} else
 		
-		if (cmd == iq_display_cmd_e.IQ_CLEAR) {	// not currently used
+		if (cmd == iq.cmd_e.IQ_CLEAR) {	// not currently used
 			iq_display_clear();
 		} else {
 			console.log('iq_display_recv: DATA UNKNOWN cmd='+ cmd +' len='+ len);
@@ -143,11 +150,15 @@ function iq_display_recv(data)
 				break;
 
 			case "cmaI":
-				iq_display_cmaI = parseFloat(param[1]);
+				iq.cmaI = parseFloat(param[1]);
 				break;
 
 			case "cmaQ":
-				iq_display_cmaQ = parseFloat(param[1]);
+				iq.cmaQ = parseFloat(param[1]);
+				break;
+
+			case "df":
+				iq.df = parseFloat(param[1]);
 				break;
 
 			default:
@@ -179,8 +190,37 @@ function iq_display_controls_setup()
       );
 
 	// FIXME
-	var draw_s = dbgUs? { 0:'points', 1:'density', 2:'s4285 pts', 3:'s4285 den' } : { 0:'points', 1:'density' };
+	//var draw_s = dbgUs? { 0:'points', 1:'density', 2:'s4285 pts', 3:'s4285 den' } : { 0:'points', 1:'density' };
+	var draw_s = { 0:'points', 1:'density' };
+	var mode_s = { 0:'IQ', 1:'carrier' };
+	var pll_s = { 0:'off', 1:'on', 2:'BPSK', 3:'QPSK', 4:'8PSK' };
 
+   var p = ext_param();
+   if (p) {
+      p = p.split(',');
+      for (var i=0, len = p.length; i < len; i++) {
+         var a = p[i];
+         //console.log('iq_display: param <'+ a +'>');
+         w3_obj_enum_data(draw_s, a.toString(), function(i, key) { iq.draw = key; });
+         w3_obj_enum_data(mode_s, a.toString(), function(i, key) { iq.mode = key; });
+         w3_obj_enum_data(pll_s, a.toString(), function(i, key) { iq.pll = key; });
+         if (a.startsWith('pbw:')) {
+            iq.pll_bw = parseInt(a.substring(4));
+         }
+      }
+   }
+   //console.log('iq_display: iq.pll='+ iq.pll +' iq.pll_bw='+ iq.pll_bw);
+   
+   /*
+   FIXME
+   pgain = (pgain != null)? parseInt(pgain) : -1;
+   if (pgain >= 0) {
+      iq_display_gain_init = pgain;
+	   iq_display_gain_cb('iq_display.gain', iq_display_gain_init);
+      w3_set_value('iq_display.gain', iq_display_gain_init);
+   }
+   */
+	
 	var controls_html =
 		w3_divs('id-iq_display-controls w3-text-white', '',
 			w3_half('', '',
@@ -190,45 +230,43 @@ function iq_display_controls_setup()
 			      w3_div('id-iq_display-adc'),
 			      w3_div('id-iq_display-gps')
 			   ),
-				w3_divs('w3-container', 'w3-tspace-8',
+				w3_divs('w3-margin-L-8', 'w3-tspace-8',
 					w3_div('w3-medium w3-text-aqua', '<b>IQ display</b>'),
 					w3_slider('Gain', 'iq_display.gain', iq_display.gain, 0, 100, 1, 'iq_display_gain_cb'),
-					w3_div('w3-show-inline-block',
-					   w3_select('', 'Draw', '', 'iq_display.draw', iq_display.draw, draw_s, 'iq_display_draw_select_cb'),
-						w3_button('|margin-left:16px', 'Clear', 'iq_display_clear_cb')
+					w3_col_percent('', '',
+					   w3_select('', 'Draw', '', 'iq_display.draw', iq_display.draw, draw_s, 'iq_display_draw_select_cb'), 36,
+					   w3_select('', 'Mode', '', 'iq.mode', iq.mode, mode_s, 'iq_display_mode_select_cb'), 36,
+					   w3_select('', 'PLL', '', 'iq.pll', iq.pll, pll_s, 'iq_display_pll_select_cb'), 27
 					),
 					w3_slider('Points', 'iq_display.points', iq_display.points, 4, 14, 1, 'iq_display_points_cb'),
-               w3_div('w3-show-inline-block',
+					w3_slider('PLL b/w', 'iq.pll_bw', iq.pll_bw, 1, 300, 1, 'iq_display_pll_bw_cb'),
+					w3_col_percent('', '',
 					   //w3_input('Clock offset', 'iq_display.offset', iq_display.offset, 'iq_display_offset_cb', '', 'w3-width-128'),
-						w3_button('', 'AM 40Hz', 'iq_display_AM_40Hz_cb')
+						w3_button('', 'Clear', 'iq_display_clear_cb'), 50,
+						w3_button('', 'AM 40Hz', 'iq_display_AM_40Hz_cb'), 50
 					),
 					'<hr '+ w3_psa('|margin:16px 0') +'>',
-               w3_div('w3-show-inline-block',
-					   w3_button('w3-css-yellow', 'IQ bal', 'iq_display_IQ_balance_cb'),
-					   w3_button('w3-css-yellow|margin-left:12px; padding:6px 10px;', 'Fcal '+ w3_icon('', 'fa-repeat'), 'iq_display_IQ_cal_jog_cb', 1),
-					   w3_button('w3-css-yellow|margin-left:12px; padding:6px 10px;', 'Fcal '+ w3_icon('', 'fa-undo'), 'iq_display_IQ_cal_jog_cb', -1)
+					w3_col_percent('', '',
+					   w3_button('w3-css-yellow', 'IQ bal', 'iq_display_IQ_balance_cb'), 33,
+					   w3_button('w3-css-yellow|margin-left:12px; padding:6px 10px;', 'Fcal '+ w3_icon('', 'fa-repeat'), 'iq_display_IQ_cal_jog_cb', 1), 33,
+					   w3_button('w3-css-yellow|margin-left:12px; padding:6px 10px;', 'Fcal '+ w3_icon('', 'fa-undo'), 'iq_display_IQ_cal_jog_cb', -1), 33
 					)
 				)
 			)
 		);
 
 	ext_panel_show(controls_html, null, null);
-	ext_set_controls_width_height(null, 330);
+	ext_set_controls_width_height(540, 340);
 
 	iq_display_canvas = w3_el('id-iq_display-canvas');
 	iq_display_canvas.ctx = iq_display_canvas.getContext("2d");
 	iq_display_imageData = iq_display_canvas.ctx.createImageData(256, 1);
 
-   var pgain = ext_param();
-   pgain = (pgain != null)? parseInt(pgain) : -1;
-   if (pgain >= 0) {
-      iq_display_gain_init = pgain;
-	   iq_display_gain_cb('iq_display.gain', iq_display_gain_init);
-      w3_set_value('iq_display.gain', iq_display_gain_init);
-   }
-	
-	iq_display_clear();
 	ext_send('SET run=1');
+	
+	// give the PLL time to settle on startup
+	setTimeout(function() { iq_display_clear() }, 500);
+	setTimeout(function() { iq_display_clear() }, 2000);
 }
 
 function iq_display_gain_cb(path, val, complete, first)
@@ -250,12 +288,42 @@ function iq_display_points_cb(path, val, complete, first)
 	iq_display_clear();
 }
 
+function iq_display_pll_bw_cb(path, val, complete, first)
+{
+   val = +val;
+   //console.log('iq_display_pll_bw_cb val='+ val);
+	w3_num_cb(path, val);
+	w3_set_label('PLL b/w '+ val +' Hz', path);
+	ext_send('SET pll_bandwidth='+ val);
+	iq_display_clear();
+}
+
 var iq_display_update_interval;
 
 function iq_display_draw_select_cb(path, idx)
 {
-	iq_display_draw = +idx;
-	ext_send('SET draw='+ iq_display_draw);
+	iq.draw = +idx;
+	ext_send('SET draw='+ iq.draw);
+	kiwi_clearInterval(iq_display_update_interval);
+	iq_display_update_interval = setInterval(iq_display_update, 250);
+	iq_display_clear();
+}
+
+function iq_display_mode_select_cb(path, idx)
+{
+	iq.mode = +idx;
+	ext_send('SET mode='+ iq.mode);
+	kiwi_clearInterval(iq_display_update_interval);
+	iq_display_update_interval = setInterval(iq_display_update, 250);
+	iq_display_clear();
+}
+
+function iq_display_pll_select_cb(path, idx)
+{
+   var exp = [0, 1, 2, 4, 8];
+	iq.pll = +idx;
+   console.log('iq_display_pll_select_cb iq.pll='+ iq.pll);
+	ext_send('SET exponent='+ exp[iq.pll]);
 	kiwi_clearInterval(iq_display_update_interval);
 	iq_display_update_interval = setInterval(iq_display_update, 250);
 	iq_display_clear();
@@ -291,7 +359,7 @@ function iq_display_IQ_balance_cb(path, val)
                'CAUTION: Only IQ balance with the<br>' +
                'antenna disconnected. Zoom in and<br>' +
                'tune to a frequency with no signals.<br>' +
-               'I = '+ (-iq_display_cmaI).toFixed(6) +'&nbsp; &nbsp; Q = '+ (-iq_display_cmaQ).toFixed(6)
+               'I = '+ (-iq.cmaI).toFixed(6) +'&nbsp; &nbsp; Q = '+ (-iq.cmaQ).toFixed(6)
             ) +
             w3_button('w3-green|margin-left:16px;', 'Confirm', 'iq_balance_confirm') +
             w3_button('w3-red|margin-left:16px;', 'Cancel', 'confirmation_panel_cancel'),
@@ -311,10 +379,10 @@ function iq_display_IQ_balance_cb(path, val)
 
 function iq_balance_confirm()
 {
-   console.log('iq_balance_confirm: INCR ADJ I='+ (-iq_display_cmaI) +' Q='+ (-iq_display_cmaQ));
-   cfg.DC_offset_I += -iq_display_cmaI;
+   console.log('iq_balance_confirm: INCR ADJ I='+ (-iq.cmaI) +' Q='+ (-iq.cmaQ));
+   cfg.DC_offset_I += -iq.cmaI;
    ext_set_cfg_param('cfg.DC_offset_I', cfg.DC_offset_I, false);
-   cfg.DC_offset_Q += -iq_display_cmaQ;
+   cfg.DC_offset_Q += -iq.cmaQ;
    ext_set_cfg_param('cfg.DC_offset_Q', cfg.DC_offset_Q, true);
    console.log('iq_balance_confirm: NEW I='+ cfg.DC_offset_I.toFixed(6) +' Q='+ cfg.DC_offset_Q.toFixed(6));
    toggle_panel('id-confirmation');
