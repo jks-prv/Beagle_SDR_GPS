b39203aa9031858c0ffd43e45d958ed9635e0e37
jks-prv <jks@jks.com>
2019-05-05 15:08:00 +1200

    add new style assertion macros

--- support/kiwi_assert.h
+++ support/kiwi_assert.h
@@ -22,6 +22,56 @@
 			}
 	#endif
 	
+#define assert_eq(v1, v2) \
+    if (!((v1) == (v2))) { \
+        printf("assertion failed: \"%s\"(%d) == \"%s\"(%d) %s line %d\n", \
+            #v1, v1, #v2, v2, __FILE__, __LINE__); \
+        panic("assert"); \
+    }
+
+#define assert_ne(v1, v2) \
+    if (!((v1) != (v2))) { \
+        printf("assertion failed: \"%s\"(%d) != \"%s\"(%d) %s line %d\n", \
+            #v1, v1, #v2, v2, __FILE__, __LINE__); \
+        panic("assert"); \
+    }
+
+#define assert_gt(v1, v2) \
+    if (!((v1) > (v2))) { \
+        printf("assertion failed: \"%s\"(%d) > \"%s\"(%d) %s line %d\n", \
+            #v1, v1, #v2, v2, __FILE__, __LINE__); \
+        panic("assert"); \
+    }
+
+#define assert_ge(v1, v2) \
+    if (!((v1) >= (v2))) { \
+        printf("assertion failed: \"%s\"(%d) >= \"%s\"(%d) %s line %d\n", \
+            #v1, v1, #v2, v2, __FILE__, __LINE__); \
+        panic("assert"); \
+    }
+
+#define assert_lt(v1, v2) \
+    if (!((v1) < (v2))) { \
+        printf("assertion failed: \"%s\"(%d) < \"%s\"(%d) %s line %d\n", \
+            #v1, v1, #v2, v2, __FILE__, __LINE__); \
+        panic("assert"); \
+    }
+
+#define assert_le(v1, v2) \
+    if (!((v1) <= (v2))) { \
+        printf("assertion failed: \"%s\"(%d) <= \"%s\"(%d) %s line %d\n", \
+            #v1, v1, #v2, v2, __FILE__, __LINE__); \
+        panic("assert"); \
+    }
+
+// e.g. array bounds checking
+#define assert_ge_lt(v1, v2, v3) \
+    if (!((v1) >= (v2) && (v1) < (v3))) { \
+        printf("assertion failed: \"%s\"(%d) >= \"%s\"(%d) AND < \"%s\"(%d) %s line %d\n", \
+            #v1, v1, #v2, v2, #v3, v3, __FILE__, __LINE__); \
+        panic("assert"); \
+    }
+
 	#define assert_dump(e) \
 		if (!(e)) { \
 			lprintf("assertion failed: \"%s\" %s line %d\n", #e, __FILE__, __LINE__); \
