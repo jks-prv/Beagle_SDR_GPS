b1efe4a692fd709534ba7a033be31311bc9cd0d2
jks-prv <jks@jks.com>
2019-06-13 09:20:34 +1200

    fix echoing of black dots on password entry

--- web/web.cpp
+++ web/web.cpp
@@ -488,7 +488,7 @@ void reload_index_params()
 	const char *gen_list_css[2][7] = {
 	    {
 		    "pkgs/font-awesome-4.6.3/css/font-awesome.min.css",
-		    "pkgs/text-security/text-security-disc.min.css",
+		    "pkgs/text-security/text-security-disc.css",
 		    "pkgs/w3.css",
 		    "kiwi/w3_ext.css",
 		    "openwebrx/openwebrx.css",
