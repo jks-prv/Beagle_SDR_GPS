58a3ae28f33b42ecc98819f44b42564e70a1f126
jks-prv <jks@jks.com>
2019-10-11 11:17:59 +1300

    v1.335: fix for GPS acquisition task lockout

--- support/coroutines.cpp
+++ support/coroutines.cpp
@@ -1140,7 +1140,7 @@ bool TaskIsChild()
 
 					if (!t->stopped && t->long_run) {
 						u4_t last_time_run = now_us - itask_last_tstart;
-						if (!itask || !itask_run || (itask_run && last_time_run < 2000)) {
+						if (!itask || !itask_run || (itask_run && last_time_run < 4000)) {
 							evNT(EC_EVENT, EV_NEXTTASK, -1, "NextTask", evprintf("OKAY for LONG RUN %s, interrupt last ran @%.6f, %d us ago",
 								task_s(t), (float) itask_last_tstart / 1000000, last_time_run));
 							//if (ev_dump) evNT(EC_DUMP, EV_NEXTTASK, ev_dump, "NextTask", evprintf("DUMP IN %.3f SEC", ev_dump/1000.0));
