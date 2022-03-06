250ce80089db102ded184ce2753ab6b6c4f40359
jks-prv <jks@jks.com>
2022-03-06 15:57:06 +1300

    add precision spec to admin_float_cb()

--- web/kiwi/admin.js
+++ web/kiwi/admin.js
@@ -3474,17 +3474,29 @@ function admin_int_cb(path, val)
    w3_set_value(path, val);   // remove any fractional part from field
 }
 
-function admin_float_cb(path, val)
+// limit precision using callback spec: 'admin_float_cb|prec'
+function admin_float_cb(path, val, first, cb_a)
 {
-	//console.log('admin_float_cb '+ path +'='+ val);
+   var prec = -1;    // default to no precision limiting applied
+	//console.log('admin_float_cb '+ path +'='+ val +' cb_a.len='+ cb_a.length);
+	if (cb_a && cb_a.length >= 2) {
+	   prec = +(cb_a[1]);
+	   if (isNaN(prec)) prec = -1;
+	   //console.log('admin_float_cb prec='+ prec);
+	}
 	val = parseFloat(val);
 	if (isNaN(val)) {
 	   // put old value back
 	   val = ext_get_cfg_param(path);
-		w3_set_value(path, val);
 	} else {
+	   if (prec != -1) {
+         var s = val.toFixed(prec);    // NB: .toFixed() does rounding
+         //console.log('admin_float_cb val('+ prec +')='+ s);
+         val = +s;
+      }
 	   ext_set_cfg_param(path, val, true);
 	}
+   w3_set_value(path, val);   // remove any non-numeric part from field
 }
 
 function admin_bool_cb(path, val, first)
