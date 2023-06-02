dbc01a9c19e8c91b953f483670402308530ad21a
jks-prv <jks-prv@users.noreply.github.com>
2023-06-02 15:23:02 +1200

    snd_send_msg() => snd_send_msg_encoded()

--- support/misc.h
+++ support/misc.h
@@ -58,8 +58,10 @@ int qsort_intcomp(const void *elem1, const void *elem2);
 float median_f(float *f, int len, float *pct_1 = NULL, float *pct_2 = NULL);
 int median_i(int *i, int len, int *pct_1 = NULL, int *pct_2 = NULL);
 
-#define SM_DEBUG	true
-#define SM_NO_DEBUG	false
+#define SM_DEBUG	    true
+#define SM_NO_DEBUG	    false
+#define SM_RX_CHAN_ALL  -1
+
 void send_msg_buf(conn_t *c, char *s, int slen);
 void send_msg(conn_t *c, bool debug, const char *msg, ...);
 void send_msg_data(conn_t *c, bool debug, u1_t dst, u1_t *bytes, int nbytes);
@@ -67,7 +69,7 @@ void send_msg_data2(conn_t *c, bool debug, u1_t dst, u1_t data2, u1_t *bytes, in
 void send_msg_mc(struct mg_connection *mc, bool debug, const char *msg, ...);
 void send_msg_encoded(conn_t *conn, const char *dst, const char *cmd, const char *fmt, ...);
 void send_msg_mc_encoded(struct mg_connection *mc, const char *dst, const char *cmd, const char *fmt, ...);
-int snd_send_msg(int rx_chan, bool debug, const char *msg, ...);
+int snd_send_msg_encoded(int rx_chan, bool debug, const char *dst, const char *cmd, const char *msg, ...);
 void snd_send_msg_data(int rc_chan, bool debug, u1_t cmd, u1_t *bytes, int nbytes);
 void input_msg_internal(conn_t *conn, const char *fmt, ...);
 
