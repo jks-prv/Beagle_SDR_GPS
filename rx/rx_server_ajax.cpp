e5beb9239d9876a5cd5b811edeada2593b270d4e
jks-prv <jks-prv@users.noreply.github.com>
2024-04-08 06:20:05 +1200

    DX label CVS import bug fix

--- rx/rx_server_ajax.cpp
+++ rx/rx_server_ajax.cpp
@@ -313,7 +313,7 @@ char *rx_server_ajax(struct mg_connection *mc, char *ip_forwarded)
                     float begin, end;
                     if (_dx_parse_csv_field(CSV_FLOAT, qs[10].str, &begin)) { rc = 22; goto fail; }
                     if (_dx_parse_csv_field(CSV_FLOAT, qs[11].str, &end)) { rc = 23; goto fail; }
-                    if ((begin != 0 || end != 0) && (begin != 0 && end != 2400))
+                    if (!(begin == 0 && end == 2400) && !(begin == 0 && end == 0))
                         sb3 = kstr_asprintf(sb3, "%s\"b0\":%.0f, \"e0\":%.0f", sb3? ", " : "", begin, end);
 
                     // N_CSV_FIELDS_SIG_BW field is optional for backward compatibility
