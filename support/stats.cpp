67b6a3d76e4a3b861f32af53807940cc1c28f2a1
jks-prv <jks-prv@users.noreply.github.com>
2024-04-11 08:43:10 +1200

    lint

--- support/stats.cpp
+++ support/stats.cpp
@@ -308,14 +308,14 @@ static void called_every_second()
             c->ext_api = false;
             c->ext_api_determined = true;
 		    web_served_clear_cache(c);
-		    cprintf(c, "API: decided connection is OKAY (%d) %s\n",
+		    cprintf(c, "API: decided connection is OKAY (served=%d) %s\n",
 		        served, c->browser? c->browser : "");
 		    continue;
 		}
 		
 		c->ext_api = c->ext_api_determined = true;
 		web_served_clear_cache(c);
-		cprintf(c, "API: decided connection is non-Kiwi app (%d) %s\n",
+		cprintf(c, "API: decided connection is non-Kiwi app (served=%d) %s\n",
 		    served, c->browser? c->browser : "");
 		//dump();
 
@@ -340,8 +340,8 @@ static void called_every_second()
                     bool hasDelimiter = (c->ident_user != NULL && strpbrk(c->ident_user, "-_.`,/+~") != NULL);
                     //bool trig = (c->type == STREAM_WATERFALL && freq_trig && hasDelimiter && c->zoom == 8);
                     bool trig = (c->type == STREAM_WATERFALL && freq_trig && c->zoom == 8);
-                    clprintf(c, "API: TRIG=%d %s(%d) f_kHz=%.3f freq_trig=%d hasDelimiter=%d z=%d\n",
-                        trig, rx_conn_type(c), c->type, f_kHz, freq_trig, hasDelimiter, c->zoom);
+                    clprintf(c, "API: TRIG=%s %s(T%d) f_kHz=%.3f freq_trig=%d hasDelimiter=%d z=%d\n",
+                        trig? "T":"F", rx_conn_type(c), c->type, f_kHz, freq_trig, hasDelimiter, c->zoom);
                     if (trig) {
                         clprintf(c, "API: non-Kiwi app fingerprint was denied connection\n");
                         kick = true;
